d2e0763e832ba5a7b352414e7334d11e6ba54531
Tony Marklove <tony@tonymarklove.net>
2011-09-30 19:32:17 +0100

    Remove unused config settings

--- config/config.php
+++ config/config.php
@@ -7,20 +7,5 @@
 	to all configs and remove it from here.
 */
 
-define('SSL_PORT', 443);
-
-	
-// These should be moved to an editable database at some point
-$GLOBALS['DATE_FORMAT_SHORT'] = 'd/m/Y';
-$GLOBALS['DATE_FORMAT_LONG'] = 'D jS F Y';
-$GLOBALS['DATE_TIME_FORMAT'] = 'd/m/Y  H:i:s';
-$GLOBALS['DB_DATE_TIME_FORMAT'] = 'Y-m-d H:i:s';
-$GLOBALS['DATE_TIME_FORMAT_LONG'] = 'D jS M Y  H:i:s';
-$GLOBALS['SYSTEM_CLOCK'] = "GMT";
-$GLOBALS['SERVER_CLOCK'] = "GMT";
-
-
 // Logging emails are sent to the following address from the live site.
 $GLOBALS['EMAIL_DEVELOPER_LOG'] = 'tony@tonymarklove.net';
-
-$GLOBALS['DB_CHARACTER_SET'] = 'utf8';
